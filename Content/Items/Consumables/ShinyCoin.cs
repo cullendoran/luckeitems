5624aded3b06effe60c6d37190a133039cf3d949
cullendoran <fasickcullen@gmail.com>
2022-08-31 18:48:49 -0400

    Added early version of final boss

---
+++ Content/Items/Consumables/ShinyCoin.cs
@@ -0,0 +1,86 @@
+﻿using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using luckeitems.Content.NPCs.LuckE;
+
+namespace luckeitems.Content.Items.Consumables
+{
+	public class ShinyCoin : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Shiny Coin");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 3;
+			ItemID.Sets.SortingPriorityBossSpawns[Type] = 12; // This helps sort inventory know that this is a boss summoning Item.
+
+			// If this would be for a vanilla boss that has no summon item, you would have to include this line here:
+			// NPCID.Sets.MPAllowedEnemies[NPCID.Plantera] = true;
+
+			// Otherwise the UseItem code to spawn it will not work in multiplayer
+		}
+		public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+			//TooltipLine line = new TooltipLine(Mod, "ShinyCoinL1", "You think you can defeat me?");
+			//line.OverrideColor = new Color(255, 87, 51);
+			//tooltips.Add(line);
+			//TooltipLine line2 = new TooltipLine(Mod, "ShinyCoinL2", "We shall see...");
+			//line2.OverrideColor = new Color(255, 87, 51);
+			//tooltips.Add(line2);
+			//TooltipLine line3 = new TooltipLine(Mod, "ShinyCoinL3", "-Creator Item-");
+			//line3.OverrideColor = new Color(Main.DiscoR, Main.DiscoG, Main.DiscoB);
+			//tooltips.Add(line3);
+			
+			TooltipLine line = new TooltipLine(Mod, "ShinyCoinNo", "You should not have this... Yet...");
+			line.OverrideColor = new Color(255, 0, 0);
+			tooltips.Add(line);
+
+        }
+
+
+        public override void SetDefaults() {
+			Item.width = 20;
+			Item.height = 30;
+			Item.maxStack = 20;
+			Item.value = 0;
+			Item.useAnimation = 30;
+			Item.useTime = 30;
+			Item.useStyle = ItemUseStyleID.HoldUp;
+			Item.consumable = true;
+		}
+
+		public override bool CanUseItem(Player player) {
+			// If you decide to use the below UseItem code, you have to include !NPC.AnyNPCs(id), as this is also the check the server does when receiving MessageID.SpawnBoss.
+			// If you want more constraints for the summon item, combine them as boolean expressions:
+			//    return !Main.dayTime && !NPC.AnyNPCs(ModContent.NPCType<MinionBossBody>()); would mean "not daytime and no MinionBossBody currently alive"
+			return !NPC.AnyNPCs(ModContent.NPCType<LuckE>()) && player.name == "LuckE";
+		}
+
+		public override bool? UseItem(Player player) {
+			if (player.whoAmI == Main.myPlayer) {
+                // If the player using the item is the client
+                // (explicitely excluded serverside here)
+				SoundEngine.PlaySound(SoundID.Coins, player.position);
+
+				int type = ModContent.NPCType<LuckE>();
+
+				if (Main.netMode != NetmodeID.MultiplayerClient) {
+					// If the player is not in multiplayer, spawn directly
+					NPC.SpawnOnPlayer(player.whoAmI, type);
+				}
+				else {
+					// If the player is in multiplayer, request a spawn
+					// This will only work if NPCID.Sets.MPAllowedEnemies[type] is true, which we set in MinionBossBody
+					NetMessage.SendData(MessageID.SpawnBoss, number: player.whoAmI, number2: type);
+				}
+			}
+
+			return true;
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+	}
+}
---
+++ Content/NPCs/LuckE/LuckE.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace luckeitems.Content.NPCs.LuckE
+{
+	// The main part of the boss, usually refered to as "body"
+	[AutoloadBossHead] // This attribute looks for a texture called "ClassName_Head_Boss" and automatically registers it as the NPC boss head icon
+	public class LuckE : ModNPC
+	{
+		public override void Load() {
+
+		}
+
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("The Creator LuckE");
+			Main.npcFrameCount[Type] = 2;
+
+			// Add this in for bosses that have a summon item, requires corresponding code in the item (See MinionBossSummonItem.cs)
+			NPCID.Sets.MPAllowedEnemies[Type] = true;
+			// Automatically group with other bosses
+			NPCID.Sets.BossBestiaryPriority.Add(Type);
+
+			// Specify the debuffs it is immune to
+			NPCDebuffImmunityData debuffData = new NPCDebuffImmunityData {
+				SpecificallyImmuneTo = new int[] {
+					BuffID.Confused // Most NPCs have this
+				}
+			};
+			NPCID.Sets.DebuffImmunitySets.Add(Type, debuffData);
+		}
+
+		public override void SetDefaults() {
+			NPC.width = 110;
+			NPC.height = 110;
+			NPC.damage = 1000;
+			NPC.defense = 70;
+			NPC.lifeMax = 1000000;
+			NPC.HitSound = SoundID.NPCHit1;
+			NPC.DeathSound = SoundID.NPCDeath1;
+			NPC.knockBackResist = 100f;
+			NPC.noGravity = true;
+			NPC.noTileCollide = true;
+			NPC.value = Item.buyPrice(gold: 5);
+			NPC.SpawnWithHigherTime(30);
+			NPC.boss = true;
+			NPC.npcSlots = 10f; // Take up open spawn slots, preventing random NPCs from spawning during the fight
+
+            NPC.CloneDefaults(NPCID.KingSlime);
+            // Don't set immunities like this as of 1.4:
+            // NPC.buffImmune[BuffID.Confused] = true;
+            // immunities are handled via dictionaries through NPCID.Sets.DebuffImmunitySets
+
+            // Custom AI, 0 is "bound town NPC" AI which slows the NPC down and changes sprite orientation towards the target
+
+			// The following code assigns a music track to the boss in a simple way.
+			if (!Main.dedServ) {
+				Music = MusicLoader.GetMusicSlot(Mod, "Assets/Music/DiscoPhial");
+			}
+		}
+
+		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry) {
+			// Sets the description of this NPC that is listed in the bestiary
+			bestiaryEntry.Info.AddRange(new List<IBestiaryInfoElement> {
+				new MoonLordPortraitBackgroundProviderBestiaryInfoElement(), // Plain black background
+				new FlavorTextBestiaryInfoElement("The Creator himself, defeating him seems to have no effect... Yet...")
+			});
+		}
+		public override void OnSpawn(IEntitySource source)
+		{
+            SoundEngine.PlaySound(SoundID.Meowmere, Entity.position);
+        }
+        public override void OnKill() {
+			// Since this hook is only ran in singleplayer and serverside, we would have to sync it manually.
+			// Thankfully, vanilla sends the MessageID.WorldData packet if a BOSS was killed automatically, shortly after this hook is ran
+
+			// If your NPC is not a boss and you need to sync the world (which includes ModSystem, check DownedBossSystem), use this code:
+			/*
+			if (Main.netMode == NetmodeID.Server) {
+				NetMessage.SendData(MessageID.WorldData);
+			}
+			*/
+		}
+
+		public override void BossLoot(ref string name, ref int potionType) {
+			// Here you'd want to change the potion type that drops when the boss is defeated. Because this boss is early pre-hardmode, we keep it unchanged
+			// (Lesser Healing Potion). If you wanted to change it, simply write "potionType = ItemID.HealingPotion;" or any other potion type
+		}
+
+		public override bool CanHitPlayer(Player target, ref int cooldownSlot) {
+			cooldownSlot = ImmunityCooldownID.Bosses; // use the boss immunity cooldown counter, to prevent ignoring boss attacks by taking damage from other sources
+			return true;
+		}
+
+		public override void FindFrame(int frameHeight) {
+			// This NPC animates with a simple "go from start frame to final frame, and loop back to start frame" rule
+			// In this case: First stage: 0-1-2-0-1-2, Second stage: 3-4-5-3-4-5, 5 being "total frame count - 1"
+			int startFrame = 0;
+			int finalFrame = 1;
+
+			int frameSpeed = 5;
+			NPC.frameCounter += 0.2f;
+			if (NPC.frameCounter > frameSpeed) {
+				NPC.frameCounter = 0;
+				NPC.frame.Y += frameHeight;
+
+				if (NPC.frame.Y > finalFrame * frameHeight) {
+					NPC.frame.Y = startFrame * frameHeight;
+				}
+			}
+		}
+	}
+}
