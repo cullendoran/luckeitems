9f40e7567cb92f782b814662896f48442499174a
cullendoran <fasickcullen@gmail.com>
2022-09-09 17:52:34 -0400

    Added combined Wood and GP Emblems and fixed Evil emblem textures

---
+++ Content/Items/Accessories/Emblem/Combined/GPCombinedEmblem.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace luckeitems.Content.Items.Accessories.Emblem.Combined
+{
+	public class GPCombinedEmblem : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("GoldenPlatinum Emblem");
+            Tooltip.SetDefault("Increases all damage by 4%\n"
+                             + "'Made from only the finest GoldenPlatinum™!'\n");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() {
+			Item.width = 28;
+			Item.height = 28;
+			Item.accessory = true;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual) {
+			// Add 2 of this class damage
+			player.GetDamage(DamageClass.Generic) += 4f;
+		}
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient<Items.Accessories.Emblem.GP.GPWarriorEmblem>();
+            recipe.AddIngredient<Items.Accessories.Emblem.GP.GPRangerEmblem>();
+            recipe.AddIngredient<Items.Accessories.Emblem.GP.GPSorcererEmblem>();
+            recipe.AddIngredient<Items.Accessories.Emblem.GP.GPSummonerEmblem>();
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Items/Accessories/Emblem/Combined/WoodCombinedEmblem.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace luckeitems.Content.Items.Accessories.Emblem.Combined
+{
+	public class WoodCombinedEmblem : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Wooden Emblem");
+			Tooltip.SetDefault("Increases all damage by 2\n"
+							 + "'Your the most desprate person I've seen.'\n");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() {
+			Item.width = 28;
+			Item.height = 28;
+			Item.accessory = true;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual) {
+			// Add 2 of this class damage
+			player.GetDamage(DamageClass.Generic).Flat += 2f;
+		}
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient<Items.Accessories.Emblem.Wood.WoodWarriorEmblem>();
+            recipe.AddIngredient<Items.Accessories.Emblem.Wood.WoodRangerEmblem>();
+            recipe.AddIngredient<Items.Accessories.Emblem.Wood.WoodSorcererEmblem>();
+            recipe.AddIngredient<Items.Accessories.Emblem.Wood.WoodSummonerEmblem>();
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+}
